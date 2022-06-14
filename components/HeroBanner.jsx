0076e09d7604d0cda604707cf359f54de8a0b075
Danislav Kostolov <danislavkostolov@gmail.com>
2022-06-14 15:47:10 +0300

    redesign

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,9 +7,9 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
+        <h1>{heroBanner.largeText1}</h1>
         <p className='beats-solo'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
         <div>
           <div className='desc'>
             <h5>Описание</h5>
